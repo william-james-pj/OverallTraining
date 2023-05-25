80a9a06cdfb635ccbd6240060b1963fda66f7096
William James <52788612+william-james-pj@users.noreply.github.com>
2023-05-25 09:19:18 -0300

    feat: initial config

---
+++ BaseApp/Extension/UIFont+Extensions.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIFont {
+    static var xxs = UIFont.systemFont(ofSize: 8)
+    static var xs = UIFont.systemFont(ofSize: 10)
+    static var sm = UIFont.systemFont(ofSize: 12)
+    static var normal = UIFont.systemFont(ofSize: 14)
+    static var md = UIFont.systemFont(ofSize: 16)
+    static var lg = UIFont.systemFont(ofSize: 18)
+    static var xl = UIFont.systemFont(ofSize: 24)
+    static var xxl = UIFont.systemFont(ofSize: 32)
+}
