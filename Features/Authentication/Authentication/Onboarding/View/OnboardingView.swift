225cc513266fcba6798276638674dc5e33d02b19
William James <52788612+william-james-pj@users.noreply.github.com>
2023-05-26 10:58:39 -0300

    feat: create onboarding view

--- Features/Authentication/Authentication/Onboarding/View/OnboardingView.swift
+++ Features/Authentication/Authentication/Onboarding/View/OnboardingView.swift
@@ -8,29 +8,128 @@
 import UIKit
 
 protocol OnboardingViewProtocol where Self: UIView {
+    var loginButtonPressed: (() -> Void)? { get set }
+    var registerButtonPressed: (() -> Void)? { get set }
 }
 
 final class OnboardingView: UIView, OnboardingViewProtocol {
     // MARK: - Constraints
     // MARK: - Variables
+    var loginButtonPressed: (() -> Void)?
+    var registerButtonPressed: (() -> Void)?
+    
     // MARK: - Components
     private let stackBase: UIStackView = {
         let stack = UIStackView()
-        stack.axis = .horizontal
-        stack.spacing = 8
+        stack.axis = .vertical
+        stack.spacing = 40
         stack.distribution = .fill
-        stack.accessibilityIdentifier = "stackBase"
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
-
+    
+    private let viewContent: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let stackContent: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 32
+        stack.distribution = .fill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private let viewImageContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let stackText: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 24
+        stack.distribution = .fill
+        return stack
+    }()
+    
     private let labelTitle: UILabel = {
         let label = UILabel()
-        label.text = "Tests"
-        label.textColor = .white
+        label.text = "A dor é temporária, mas a glória é para sempre"
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.textColor = .textColor
+        label.font = .systemFont(ofSize: 22, weight: .bold)
         return label
     }()
-
+    
+    private let labelDescription: UILabel = {
+        let label = UILabel()
+        label.text = "Treinos prontos e personalizados: a combinação ideal para alcançar seu melhor desempenho."
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .textColor
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        return label
+    }()
+    
+    private let viewContentAux: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let viewFooter: UIView = {
+        let view = UIView()
+        view.backgroundColor = .primaryColor
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 24
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let stackFooter: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 16
+        stack.distribution = .equalSpacing
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private let buttonLogin: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.titleAlignment = .center
+        config.baseBackgroundColor = .cardColor
+        config.baseForegroundColor = .textColor
+        config.buttonSize = .large
+        
+        var container = AttributeContainer()
+        container.font = .systemFont(ofSize: 16, weight: .bold)
+        config.attributedTitle = AttributedString("Comece agora", attributes: container)
+        
+        let button = UIButton()
+        button.configuration = config
+        return button
+    }()
+    
+    private let buttonRegister: UIButton = {
+        var config = UIButton.Configuration.plain()
+        config.titleAlignment = .center
+        config.baseForegroundColor = .cardColor
+        config.buttonSize = .small
+        
+        var container = AttributeContainer()
+        container.font = .systemFont(ofSize: 14, weight: .semibold)
+        config.attributedTitle = AttributedString("Continuar", attributes: container)
+        
+        let button = UIButton()
+        button.configuration = config
+        return button
+    }()
+    
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -43,23 +142,67 @@ final class OnboardingView: UIView, OnboardingViewProtocol {
 
     // MARK: - Setup
     private func setupView() {
-        backgroundColor = UIColor.primaryColor
-
+        backgroundColor = .backgroundColor
+        
+        setupActions()
         buildHierarchy()
         buildConstraints()
+    }
+    
+    private func setupActions() {
+        buttonLogin.addTarget(self, action: #selector(loginButtonAction), for: .touchUpInside)
+        buttonRegister.addTarget(self, action: #selector(registerButtonAction), for: .touchUpInside)
+    }
+    
+    // MARK: - Actions
+    @objc private func loginButtonAction() {
+        loginButtonPressed?()
+    }
+    
+    @objc private func registerButtonAction() {
+        registerButtonPressed?()
     }
 
     // MARK: - Methods
     private func buildHierarchy() {
         addSubview(stackBase)
+        stackBase.addArrangedSubview(viewContent)
+        
+        viewContent.addSubview(stackContent)
+        stackContent.addArrangedSubview(viewImageContainer)
+        
+        stackContent.addArrangedSubview(stackText)
+        stackText.addArrangedSubview(labelTitle)
+        stackText.addArrangedSubview(labelDescription)
+        
+        stackContent.addArrangedSubview(viewContentAux)
+        
+        stackBase.addArrangedSubview(viewFooter)
+        viewFooter.addSubview(stackFooter)
+        stackFooter.addArrangedSubview(buttonRegister)
+        stackFooter.addArrangedSubview(buttonLogin)
     }
 
     private func buildConstraints() {
         NSLayoutConstraint.activate([
-            stackBase.topAnchor.constraint(equalTo: topAnchor),
+            stackBase.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             stackBase.leadingAnchor.constraint(equalTo: leadingAnchor),
             stackBase.trailingAnchor.constraint(equalTo: trailingAnchor),
-            stackBase.bottomAnchor.constraint(equalTo: bottomAnchor)
+            stackBase.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            stackContent.topAnchor.constraint(equalTo: viewContent.topAnchor),
+            stackContent.leadingAnchor.constraint(equalTo: viewContent.leadingAnchor, constant: 24),
+            stackContent.trailingAnchor.constraint(equalTo: viewContent.trailingAnchor, constant: -24),
+            stackContent.bottomAnchor.constraint(equalTo: viewContent.bottomAnchor),
+            
+            viewImageContainer.heightAnchor.constraint(equalToConstant: 280),
+            
+            viewFooter.heightAnchor.constraint(equalToConstant: 130),
+            
+            stackFooter.topAnchor.constraint(equalTo: viewFooter.topAnchor, constant: 32),
+            stackFooter.leadingAnchor.constraint(equalTo: viewFooter.leadingAnchor, constant: 24),
+            stackFooter.trailingAnchor.constraint(equalTo: viewFooter.trailingAnchor, constant: -24),
+            stackFooter.bottomAnchor.constraint(equalTo: viewFooter.safeAreaLayoutGuide.bottomAnchor, constant: -16)
         ])
     }
 }
